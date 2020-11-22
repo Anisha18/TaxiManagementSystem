fada62211a13c7282cbdac92de00fde563103e8b
Anisha Dsouza <anishadsouza2000@gmail.com>
2020-11-22 14:07:07 +0530

    Initial commit

---
+++ dbms_taxi/config.py
@@ -0,0 +1,8 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgresql:postgres@localhost:5432/taxi_db" 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
